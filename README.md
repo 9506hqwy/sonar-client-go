510ac8b4e63d7e2822f7dd9b426c9bf9ab9bdc36
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-06-14 13:12:25 +0900

    API のバージョンを v9.9 に固定した。

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This repository provides SonarQube server Web API v1 client.
 
+## API Versions
+
+* [v9.9](https://github.com/9506hqwy/sonar-client-go/tree/main)
+
 ## Generating
 
 Generate REST API client.
--- scripts/gen.sh
+++ scripts/gen.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+VERSION=v9.9
+
 BASE_DIR=$(dirname $(dirname "$0"))
 TMP_DIR=/tmp
 
 curl -fsSL \
     -o "${TMP_DIR}/openapi.yml" \
-    https://github.com/9506hqwy/openapi-spec-sonarqube-v1/raw/refs/heads/main/openapi.yml
+    "https://github.com/9506hqwy/openapi-spec-sonarqube-v1/raw/refs/heads/${VERSION}/openapi.yml"
 
 go tool oapi-codegen -config "${BASE_DIR}/cfg.yml" "${TMP_DIR}/openapi.yml"
