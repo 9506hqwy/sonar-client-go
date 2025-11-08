0cce4338c43218b952133e86f74c2303670e8774
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-11-08 13:07:07 +0900

    タグに jsonschema を追加した。

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -7,7 +7,14 @@ BASE_DIR=$(dirname "$(dirname "$0")")
 TMP_DIR=/tmp
 
 curl -fsSL \
-    -o "${TMP_DIR}/openapi.yml" \
+    -o "${TMP_DIR}/openapi-orig.yml" \
     "https://github.com/9506hqwy/openapi-spec-sonarqube-v1/raw/refs/heads/${VERSION}/openapi.yml"
+
+# shellcheck disable=SC2016
+cat "${TMP_DIR}/openapi-orig.yml" | \
+    yq '.. |= select(has("parameters")) |= .parameters |= map(.description as $x | .x-oapi-codegen-extra-tags.jsonschema = "description=" + $x)' | \
+    yq '.. |= select(has("x-oapi-codegen-extra-tags")) |= .x-oapi-codegen-extra-tags |= .jsonschema |= trim' | \
+    yq '.. |= select(has("x-oapi-codegen-extra-tags")) |= .x-oapi-codegen-extra-tags |= .jsonschema |= sub("\"", "\\\"")' \
+    > "${TMP_DIR}/openapi.yml"
 
 go tool oapi-codegen -config "${BASE_DIR}/cfg.yml" "${TMP_DIR}/openapi.yml"
